06de3af1086c54368aa07b62239470a66f21f8fa
宗畦 黃 <hsi040@hotmail.com>
2018-05-31 14:59:15 +0800

    new Basic Line Bot

---
+++ WebApplication3/Controllers/LineChatController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class LineChatController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult POST()
+        {
+            string ChannelAccessToken = "6rBvWdMhPjnI6rNHf/oUxRdviI9j+/GF1EAD5O27UP95CMxnq8lCpkqACan/+58GnxYG8YYojCHIPdLwnX9leFjmv9vRNF9zuCdfNP2bCz+91mnffE2y+JLjvfglpxWMhfFdFEqSqa7GTbPpHJz3YgdB04t89/1O/w1cDnyilFU=";
+
+            try
+            {
+                //取得 http Post RawData(should be JSON)
+                string postData = Request.Content.ReadAsStringAsync().Result;
+                //剖析JSON
+                var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
+                //回覆訊息
+                string Message;
+                Message = "你說了:" + ReceivedMessage.events[0].message.text;
+                //回覆用戶
+                isRock.LineBot.Utility.ReplyMessage(ReceivedMessage.events[0].replyToken, Message, ChannelAccessToken);
+                //回覆API OK
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return Ok();
+            }
+        }
+    }
+}
---
+++ WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            isRock.LineBot.Bot bot = new isRock.LineBot.Bot
+                ("自己的 Channel access token");
+            bot.PushMessage("Your user ID", "Test");
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            isRock.LineBot.Bot bot = new isRock.LineBot.Bot
+                ("自己的 Channel access token");
+            bot.PushMessage("Your user ID", 1, 113);
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            isRock.LineBot.Bot bot = new isRock.LineBot.Bot
+                ("自己的 Channel access token");
+            bot.PushMessage("Your user ID", new Uri("https://s2.fafa01.com/uploads/06/2/310228/14973227133525.jpg"));
+        }
+    }
+}
